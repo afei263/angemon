0db2b0852c7a148b4d5a97cf9f3bd242f192e8ad
Zeray Rice <fanzeyi1994@gmail.com>
2011-11-25 17:09:18 +0800

    finish install

--- admin.py
+++ admin.py
@@ -2,6 +2,8 @@
 
 import hashlib
 
+import tornado.web
+
 from base import BaseHandler
 
 class InstallHandler(BaseHandler):
@@ -9,6 +11,21 @@ class InstallHandler(BaseHandler):
         count = self.db.get("SELECT count(1) as count FROM User")
         if count['count'] != 0:
             raise tornado.web.HTTPError(404)
+        self.render('install.html', usr = None, error = 0)
+    def post(self):
+        usr = self.get_argument("usr", default = None)
+        pwd = self.get_argument("pwd", default = None)
+        nickname = self.get_argument("nickname", default = "Angemon")
+        if not usr or not pwd:
+            self.render('install.html', usr = usr, error = 1)
+        if len(usr) < 4 or len(pwd) < 5:
+            self.render('install.html', usr = usr, error = 2)
+        if len(usr) > 15 or len(pwd) > 15:
+            self.render('install.html', usr = usr, error = 3)
+        auth = hashlib.sha1(str(usr) + str(pwd) + "angemon").hexdigest()
+        self.db.execute("INSERT INTO User(Username, Auth, Nickname) VALUES (%s, %s, %s)", \
+                        usr, auth, nickname)
+        self.redirect('/signin')
 
 class SigninHandler(BaseHandler):
     def get(self):
